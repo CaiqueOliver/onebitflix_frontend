23202b89fbbc702caaf3481c823042df26b5411a
Caique Oliveira <caiquesilva.soluevo@repassa.com.br>
2023-07-19 17:26:02 -0300

    consertando player

--- src/components/common/slideCard/index.tsx
+++ src/components/common/slideCard/index.tsx
@@ -9,7 +9,7 @@ interface props {
 const SlideCard = ({ course }: props) => {
   return (
     <>
-    <Link href={`/course/${course.id}`}>    
+    <Link href={`/course/${course.id}`} className={styles.link}>    
       <div className={styles.slide}>
         <img
           src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
