34522c5e2dd8bee6538836f603310985e6449a83
Caique Oliveira <caiquesilva.soluevo@repassa.com.br>
2023-07-18 21:02:43 -0300

    finalizando curso

--- pages/course/[id].tsx
+++ pages/course/[id].tsx
@@ -10,132 +10,158 @@ import EpisodeList from "@/src/components/episodeList";
 import Footer from "@/src/components/common/footer";
 
 const CoursePage = function () {
-  const router = useRouter();
-  const { id } = router.query;
-  const [course, setCourse] = useState<CourseType>();
-  const [liked, setLiked] = useState(false);
-  const [favorited, setFavorited] = useState(false);
-  const getCourse = async () => {
-    if (typeof id !== "string") return;
-    const res = await courseService.getEpisodes(id);
-    if (res.status === 200) {
-      setCourse(res.data);
-      setLiked(res.data.liked);
-      setFavorited(res.data.favorited);
-    }
-  };
+	const router = useRouter();
+	const { id } = router.query;
+	const [course, setCourse] = useState<CourseType>();
+	const [liked, setLiked] = useState(false);
+	const [favorited, setFavorited] = useState(false);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getCourse();
-  }, [id]);
+	useEffect(() => {
+		if (!sessionStorage.getItem("onebitflix-token")) {
+			router.push("/login");
+		} else {
+			setLoading(false);
+		}
+	}, []);
 
-  const handleLikeCourse = async () => {
-    if (typeof id !== "string") return;
-    if (liked === true) {
-      await courseService.unlike(id);
-      setLiked(false);
-    } else {
-      await courseService.like(id);
-      setLiked(true);
-    }
-  };
-  const handleFavoriteCourse = async () => {
-    if (typeof id !== "string") return;
-    if (favorited === true) {
-      await courseService.removeFav(id);
-      setFavorited(false);
-    } else {
-      await courseService.addToFav(id);
-      setFavorited(true);
-    }
-  };
+	if (loading) {
+		return <PageSpinner />;
+	}
 
-  if (course === undefined) return <PageSpinner />;
+	const getCourse = async () => {
+		if (typeof id !== "string") return;
+		const res = await courseService.getEpisodes(id);
+		if (res.status === 200) {
+			setCourse(res.data);
+			setLiked(res.data.liked);
+			setFavorited(res.data.favorited);
+		}
+	};
 
-  return (
-    <>
-      <Head>
-        <title>Onebitflix - {course?.name}</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-      </Head>
-      <main>
-        <div
-          style={{
-            backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course?.thumbnailUrl})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            minHeight: "450px",
-          }}
-        >
-          <HeaderAuth />
-        </div>
-        <Container className={styles.courseInfo}>
-          <p className={styles.courseTitle}>{course?.name}</p>
-          <p className={styles.courseDescription}>{course?.synopsis}</p>
-          <Button
-            outline
-            className={styles.courseBtn}
-            disabled={course?.episodes?.length === 0 ? true : false}
-          >
-            Assistir Agora!
-            <img
-              src="/buttonPlay.svg"
-              alt="buttonImg"
-              className={styles.buttonImg}
-            />
-          </Button>
-          <div className={styles.interactions}>
-            {favorited === false ? (
-              <img
-                src="/course/iconLike.svg"
-                alt="icon Like"
-                className={styles.interactionImages}
-                onClick={handleFavoriteCourse}
-              />
-            ) : (
-              <img
-                src="/course/iconLiked.svg"
-                alt="icon Like"
-                className={styles.interactionImages}
-                onClick={handleFavoriteCourse}
-              />
-            )}
-            {liked === false ? (
-              <img
-                src="/course/iconAddFav.svg"
-                alt="icon Like"
-                className={styles.interactionImages}
-                onClick={handleLikeCourse}
-              />
-            ) : (
-              <img
-                src="/course/iconFavorited.svg"
-                alt="icon Like"
-                className={styles.interactionImages}
-                onClick={handleLikeCourse}
-              />
-            )}
-          </div>
-        </Container>
-        <Container className={styles.episodeInfo}>
-          <p className={styles.episodeDivision}>Episódios</p>
-          <p className={styles.episodeLenght}>
-            {course?.episodes?.length} Episódios
-          </p>
-          {course?.episodes?.length === 0 ? (
-            <p>
-              <strong>Não temos episódios ainda. &#x1F606;</strong>
-            </p>
-          ) : (
-            course?.episodes?.map((episode) => (
-              <EpisodeList key={episode.id} episode={episode} />
-            ))
-          )}
-        </Container>
-        <Footer />
-      </main>
-    </>
-  );
+	useEffect(() => {
+		getCourse();
+	}, [id]);
+
+	const handleLikeCourse = async () => {
+		if (typeof id !== "string") return;
+		if (liked === true) {
+			await courseService.unlike(id);
+			setLiked(false);
+		} else {
+			await courseService.like(id);
+			setLiked(true);
+		}
+	};
+	const handleFavoriteCourse = async () => {
+		if (typeof id !== "string") return;
+		if (favorited === true) {
+			await courseService.removeFav(id);
+			setFavorited(false);
+		} else {
+			await courseService.addToFav(id);
+			setFavorited(true);
+		}
+	};
+
+	if (course === undefined) return <PageSpinner />;
+
+	return (
+		<>
+			<Head>
+				<title>Onebitflix - {course?.name}</title>
+				<link
+					rel="shortcut icon"
+					href="/favicon.svg"
+					type="image/x-icon"
+				/>
+			</Head>
+			<main>
+				<div
+					style={{
+						backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course?.thumbnailUrl})`,
+						backgroundSize: "cover",
+						backgroundPosition: "center",
+						minHeight: "450px",
+					}}
+				>
+					<HeaderAuth />
+				</div>
+				<Container className={styles.courseInfo}>
+					<p className={styles.courseTitle}>{course?.name}</p>
+					<p className={styles.courseDescription}>
+						{course?.synopsis}
+					</p>
+					<Button
+						outline
+						className={styles.courseBtn}
+						disabled={course?.episodes?.length === 0 ? true : false}
+					>
+						Assistir Agora!
+						<img
+							src="/buttonPlay.svg"
+							alt="buttonImg"
+							className={styles.buttonImg}
+						/>
+					</Button>
+					<div className={styles.interactions}>
+						{favorited === false ? (
+							<img
+								src="/course/iconLike.svg"
+								alt="icon Like"
+								className={styles.interactionImages}
+								onClick={handleFavoriteCourse}
+							/>
+						) : (
+							<img
+								src="/course/iconLiked.svg"
+								alt="icon Like"
+								className={styles.interactionImages}
+								onClick={handleFavoriteCourse}
+							/>
+						)}
+						{liked === false ? (
+							<img
+								src="/course/iconAddFav.svg"
+								alt="icon Like"
+								className={styles.interactionImages}
+								onClick={handleLikeCourse}
+							/>
+						) : (
+							<img
+								src="/course/iconFavorited.svg"
+								alt="icon Like"
+								className={styles.interactionImages}
+								onClick={handleLikeCourse}
+							/>
+						)}
+					</div>
+				</Container>
+				<Container className={styles.episodeInfo}>
+					<p className={styles.episodeDivision}>Episódios</p>
+					<p className={styles.episodeLenght}>
+						{course?.episodes?.length} Episódios
+					</p>
+					{course?.episodes?.length === 0 ? (
+						<p>
+							<strong>
+								Não temos episódios ainda. &#x1F606;
+							</strong>
+						</p>
+					) : (
+						course?.episodes?.map((episode) => (
+							<EpisodeList
+								key={episode.id}
+								episode={episode}
+								course={course}
+							/>
+						))
+					)}
+				</Container>
+				<Footer />
+			</main>
+		</>
+	);
 };
 
 export default CoursePage;
--- src/components/episodeList/index.tsx
+++ src/components/episodeList/index.tsx
@@ -1,38 +1,53 @@
-import { EpisodeType } from "@/src/services/courseService";
+import { CourseType, EpisodeType } from "@/src/services/courseService";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
 
 interface props {
-  episode: EpisodeType;
+	episode: EpisodeType;
+	course: CourseType;
 }
 
-const EpisodeList = function ({ episode }: props) {
-  const handleSecondsToMin = (totalSeconds: number) => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-    function toString(num: number) {
-      return num.toString().padStart(2, "0");
-    }
+const EpisodeList = function ({ episode, course }: props) {
+	const router = useRouter();
+	const handleSecondsToMin = (totalSeconds: number) => {
+		const minutes = Math.floor(totalSeconds / 60);
+		const seconds = totalSeconds % 60;
+		function toString(num: number) {
+			return num.toString().padStart(2, "0");
+		}
 
-    const result = `${toString(minutes)} ${toString(seconds)}`;
-    return result;
-  };
+		const result = `${toString(minutes)} ${toString(seconds)}`;
+		return result;
+	};
 
-  return (
-    <>
-      <div className={styles.episodeCard}>
-        <div className={styles.episodeOrderTime}>
-          <p className={styles.episodeOrder}>Episódio N° {episode.order}</p>
-          <p className={styles.episodeTime}>
-            {handleSecondsToMin(episode.secondsLong)}
-          </p>
-        </div>
-        <div className={styles.episodeTitleDescription}>
-          <p className={styles.episodeTitle}>{episode.name}</p>
-          <p className={styles.episodeDescription}>{episode.synopsis}</p>
-        </div>
-      </div>
-    </>
-  );
+	const handleEpisodePlayer = () => {
+		router.push(
+			`/course/episode/${episode.order - 1}?courseid=${
+				course.id
+			}&episodeid=${episode.id}`
+		);
+	};
+
+	return (
+		<>
+			<div className={styles.episodeCard} onClick={handleEpisodePlayer}>
+				<div className={styles.episodeOrderTime}>
+					<p className={styles.episodeOrder}>
+						Episódio N° {episode.order}
+					</p>
+					<p className={styles.episodeTime}>
+						{handleSecondsToMin(episode.secondsLong)}
+					</p>
+				</div>
+				<div className={styles.episodeTitleDescription}>
+					<p className={styles.episodeTitle}>{episode.name}</p>
+					<p className={styles.episodeDescription}>
+						{episode.synopsis}
+					</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default EpisodeList;
